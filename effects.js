1a8ad6bec08eaf5506955e3f7d418dbd2ee97e64
WasabiMushyPeas <Jackson.McDowell@gmail.com>
2023-09-07 16:43:06 -0600

    basic laser

---
+++ effects.js
@@ -0,0 +1,33 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@#$%^&*()_+`1234567890-=[]{}|;':,./<>?";
+
+let interval = null;
+let onHeader = true;
+
+document.querySelector("h1").onmouseover = event => {
+    onHeader = true;
+    let iteration = 0;
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+
+        event.target.innerText = event.target.innerText.split("").map((letter, index) => {
+            if (index < iteration && onHeader) {
+                return event.target.dataset.value[index];
+            }
+            if (event.target.dataset.value[index] == " " && onHeader) {
+                return " ";
+            }
+
+            if (onHeader) {
+                return letters[Math.floor(Math.random() * 26)]
+            }
+
+
+        }).join("");
+        if (iteration >= event.target.dataset.value.length) {
+            clearInterval(interval);
+        }
+        iteration += 1 / 3;
+    }, 30);
+
+}
